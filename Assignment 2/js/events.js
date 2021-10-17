f5106c970e87d9e441801f9a2bcc99fe6a1cfa55
cooperchestney <65068863+cooperchestney@users.noreply.github.com>
2021-10-17 20:53:04 +1100

    Add files via upload

---
+++ Assignment 2/js/events.js
@@ -0,0 +1,128 @@
+// JavaScript source code
+
+function showOverall() {
+	var overallSummary = document.getElementById('overall');
+	var chemistrySummary = document.getElementById('chemistry');
+	var economicsSummary = document.getElementById('economics');
+	var literatureSummary = document.getElementById('literature');
+	var medicineSummary = document.getElementById('medicine');
+	var peaceSummary = document.getElementById('peace');
+	var physicsSummary = document.getElementById('physics');
+
+	overallSummary.style.display = "block";
+	chemistrySummary.style.display = "none";
+	economicsSummary.style.display = "none";
+	literatureSummary.style.display = "none";
+	medicineSummary.style.display = "none";
+	peaceSummary.style.display = "none";
+	physicsSummary.style.display = "none";
+}
+
+function showChemistry() {
+	var overallSummary = document.getElementById('overall');
+	var chemistrySummary = document.getElementById('chemistry');
+	var economicsSummary = document.getElementById('economics');
+	var literatureSummary = document.getElementById('literature');
+	var medicineSummary = document.getElementById('medicine');
+	var peaceSummary = document.getElementById('peace');
+	var physicsSummary = document.getElementById('physics');
+
+	overallSummary.style.display = "none";
+	chemistrySummary.style.display = "block";
+	economicsSummary.style.display = "none";
+	literatureSummary.style.display = "none";
+	medicineSummary.style.display = "none";
+	peaceSummary.style.display = "none";
+	physicsSummary.style.display = "none";
+}
+
+function showEconomics() {
+	var overallSummary = document.getElementById('overall');
+	var chemistrySummary = document.getElementById('chemistry');
+	var economicsSummary = document.getElementById('economics');
+	var literatureSummary = document.getElementById('literature');
+	var medicineSummary = document.getElementById('medicine');
+	var peaceSummary = document.getElementById('peace');
+	var physicsSummary = document.getElementById('physics');
+
+	overallSummary.style.display = "none";
+	chemistrySummary.style.display = "none";
+	economicsSummary.style.display = "block";
+	literatureSummary.style.display = "none";
+	medicineSummary.style.display = "none";
+	peaceSummary.style.display = "none";
+	physicsSummary.style.display = "none";
+}
+
+function showLiterature() {
+	var overallSummary = document.getElementById('overall');
+	var chemistrySummary = document.getElementById('chemistry');
+	var economicsSummary = document.getElementById('economics');
+	var literatureSummary = document.getElementById('literature');
+	var medicineSummary = document.getElementById('medicine');
+	var peaceSummary = document.getElementById('peace');
+	var physicsSummary = document.getElementById('physics');
+
+	overallSummary.style.display = "none";
+	chemistrySummary.style.display = "none";
+	economicsSummary.style.display = "none";
+	literatureSummary.style.display = "block";
+	medicineSummary.style.display = "none";
+	peaceSummary.style.display = "none";
+	physicsSummary.style.display = "none";
+}
+
+function showMedicine() {
+	var overallSummary = document.getElementById('overall');
+	var chemistrySummary = document.getElementById('chemistry');
+	var economicsSummary = document.getElementById('economics');
+	var literatureSummary = document.getElementById('literature');
+	var medicineSummary = document.getElementById('medicine');
+	var peaceSummary = document.getElementById('peace');
+	var physicsSummary = document.getElementById('physics');
+
+	overallSummary.style.display = "none";
+	chemistrySummary.style.display = "none";
+	economicsSummary.style.display = "none";
+	literatureSummary.style.display = "none";
+	medicineSummary.style.display = "block";
+	peaceSummary.style.display = "none";
+	physicsSummary.style.display = "none";
+}
+
+function showPeace() {
+	var overallSummary = document.getElementById('overall');
+	var chemistrySummary = document.getElementById('chemistry');
+	var economicsSummary = document.getElementById('economics');
+	var literatureSummary = document.getElementById('literature');
+	var medicineSummary = document.getElementById('medicine');
+	var peaceSummary = document.getElementById('peace');
+	var physicsSummary = document.getElementById('physics');
+
+	overallSummary.style.display = "none";
+	chemistrySummary.style.display = "none";
+	economicsSummary.style.display = "none";
+	literatureSummary.style.display = "none";
+	medicineSummary.style.display = "none";
+	peaceSummary.style.display = "block";
+	physicsSummary.style.display = "none";
+}
+
+function showPhysics() {
+	var overallSummary = document.getElementById('overall');
+	var chemistrySummary = document.getElementById('chemistry');
+	var economicsSummary = document.getElementById('economics');
+	var literatureSummary = document.getElementById('literature');
+	var medicineSummary = document.getElementById('medicine');
+	var peaceSummary = document.getElementById('peace');
+	var physicsSummary = document.getElementById('physics');
+
+	overallSummary.style.display = "none";
+	chemistrySummary.style.display = "none";
+	economicsSummary.style.display = "none";
+	literatureSummary.style.display = "none";
+	medicineSummary.style.display = "none";
+	peaceSummary.style.display = "none";
+	physicsSummary.style.display = "block";
+}
+
